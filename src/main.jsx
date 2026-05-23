e29d159bbf0dff0e09b04580e13c7e8797ac9ce2
agenciafenixwarriors <agenciafenixwarriors@gmail.com>
2026-05-23 16:15:41 -0300

    Fix router jsx import

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import { router } from "./routes/router";
-
-import { AuthProvider } from "./contexts/AuthContext";
+import router from "./routes/router.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
