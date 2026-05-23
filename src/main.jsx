231a12f1cbf45067e13531f3d6403638bdc513c1
agenciafenixwarriors <agenciafenixwarriors@gmail.com>
2026-05-23 16:09:38 -0300

    Fix router path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
-import {
-  RouterProvider
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes/router";
 
@@ -12,18 +8,10 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <AuthProvider>
-
       <RouterProvider router={router} />
-
     </AuthProvider>
-
   </React.StrictMode>
-
 );
