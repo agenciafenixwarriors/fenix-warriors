4f998bde92e20e5c2d5db0cabcbdf4ec05ce39e1
agenciafenixwarriors <agenciafenixwarriors@gmail.com>
2026-08-30 00:58:27 -0300

    Load Nova Era operational styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-
+import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 
 import "./index.css";
+import "./nova-era.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
